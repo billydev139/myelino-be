0c81b6ec4471bcc551928e2d38f58fa2e32b9ed2
danishriazsoftmind <danishriaz@softmindsol.com>
2024-05-25 17:34:01 +0500

    remove body-parser

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
 import public_route from "./src/routes/public.js";
-import bodyparser from "body-parser";
+//import bodyparser from "body-parser";
 export const app = express();
 
 const corsOptions = {
   origin: "https://myelino.com", // Replace with your frontend domain
   methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: ["Content-Type", "Authorization"],
+  //allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
@@ -21,12 +21,12 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyparser.urlencoded({ extended: true }));
-app.use(bodyparser.json());
-app.use(
-  bodyparser.json({
-    limit: "50mb",
-  })
-);
+//app.use(bodyparser.urlencoded({ extended: true }));
+//app.use(bodyparser.json());
+// app.use(
+//   bodyparser.json({
+//     limit: "50mb",
+//   })
+// );
 
 app.use("/public", public_route);
